072e78bab1752ec090118cc155fcea8ec544b3b4
Robert Gravina <robert@gravina.com>
2012-09-15 10:42:24 +0900

    Initial Rails generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chuhi::Application.config.secret_token = '0cd0c013f5502e1a949b697eecd1f657318b0b74a5d6a38462f8ad2833e7befc708196b1e7b26684307ed68368f6f8a412d45276e7cead9e8d6c32cb1975bff1'
