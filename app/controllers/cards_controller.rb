3053259994493d39fd40693c724af76e9347e9db
Robert Gravina <robert@gravina.com>
2012-11-04 01:43:29 +0900

    Skip the untested stack when inputing words directly

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,6 +7,8 @@ class CardsController < ApplicationController
     @word = Word.new(:kanji => params[:word][:kanji], :kana => params[:word][:kana], :translation => params[:word][:translation])
     if @word.save
       current_user.words << @word
+      # add the word to the review cycle right away
+      current_user.right_answer_for!(@word)
       flash[:success] = "Card added to your deck."
       redirect_to(:controller => :deck)
     else
