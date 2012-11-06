6245e59d95db6625900df99ea7d326bb6961d010
Robert Gravina <robertg@reallyenglish.com>
2012-11-06 11:38:42 +0900

    Redirect to card new if deck is empty. Upgraded okubo to fix bug.

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -2,5 +2,6 @@ class DeckController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    redirect_to(:controller => :cards, :action => :new) if current_user.words.count == 0
   end
 end
